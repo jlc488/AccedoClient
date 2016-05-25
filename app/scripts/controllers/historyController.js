924007ba4d9d4b05ad91dec7b71300ff33e6c6b6
Sin-Kang <kangsin@cspmedia.net>
2016-05-25 14:32:09 +0900

    remove total count in historymodel view

--- app/scripts/controllers/historyController.js
+++ app/scripts/controllers/historyController.js
@@ -4,7 +4,7 @@ angular.module('accedoClientApp')
 .controller('HistoryCtrl', function($scope, HistoryService){
 
   HistoryService.FindAll().then(function(ret){
-    $scope.totalCount = ret.totalCount;
+  //  $scope.totalCount = ret.totalCount;
     $scope.result = ret;
   }, function( ret ){
     console.log(ret);
